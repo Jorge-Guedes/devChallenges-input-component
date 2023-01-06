67dc8ac15f391874db0486dbff874faf087cb026
Jorge Guedes <jorge_guedes00@hotmail.com>
2023-01-06 11:41:25 -0300

    added input component creation

--- src/pages/Inputs.jsx
+++ src/pages/Inputs.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
 import Head from "../Components/Helper/Head";
-import Footer from "../Components/Footer";
 import Input from "../Components/Input/Input";
+import Footer from "../Components/Footer";
+
+import InputText from "../Components/InputText";
+import InputSelect from "../Components/InputSelect";
+import InputBox from "../Components/InputBox";
+
+import useForm from "../Hooks/useForm";
 
 import styles from "./Inputs.module.css";
+import styles2 from "../Components/customize.module.css";
 
 const Inputs = () => {
+  const label = useForm();
+  const placeholder = useForm();
+  const error = useForm();
+  const size = useForm();
+  const disabled = useForm();
+
   return (
     <main className="container">
       <Head title="Inputs" description="Components page dedicated to Inputs" />
@@ -23,7 +36,7 @@ const Inputs = () => {
         <Input helperText="Some interesting text" error />
         <Input startIcon="phone" classGrid="column_2" />
         <Input endIcon="lock" />
-        <Input value="Text" classGrid="column_3" />
+        <Input value="text" classGrid="column_3" />
         <Input size="sm" />
         <Input size="md" />
         <Input size="lg" />
@@ -39,6 +52,29 @@ const Inputs = () => {
           https://material.io/resources/icons/?style=round
         </a>
       </p>
+
+      <section className={styles2.customizeComponent}>
+        <h2 className="title">Customize</h2>
+        <div className={styles2.componentGenerate}>
+          <Input
+            hiddenTag
+            label={label.value}
+            placeholder={placeholder.value}
+            error={error.box}
+            disabled={disabled.box}
+            size={size.value}
+          />
+        </div>
+        <div className={styles2.containerInputs}>
+          <InputText label="label" {...label} />
+          <InputText label="placeholder" {...placeholder} />
+          <div className={styles2.input_group}>
+            <InputBox label="error" {...error} />
+            <InputBox label="disabled" {...disabled} />
+          </div>
+          <InputSelect label="size" {...size} sizes />
+        </div>
+      </section>
 
       <Footer />
     </main>
